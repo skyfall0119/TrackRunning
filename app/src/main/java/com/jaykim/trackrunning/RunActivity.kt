849d94db2e89f74b94c6ce217737bb84f5610099
Jay Kim <jaykim.coding@gmail.com>
2023-08-09 17:18:07 +0900

    Run DataClass Structure Update

--- app/src/main/java/com/jaykim/trackrunning/RunActivity.kt
+++ app/src/main/java/com/jaykim/trackrunning/RunActivity.kt
@@ -2,10 +2,22 @@ package com.jaykim.trackrunning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jaykim.trackrunning.databinding.ActivityRunBinding
 
 class RunActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRunBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_run)
+
+        binding = ActivityRunBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
     }
 }
--- app/src/main/java/com/jaykim/trackrunning/SingleRun.kt
+++ app/src/main/java/com/jaykim/trackrunning/SingleRun.kt
@@ -1,16 +1,14 @@
 package com.jaykim.trackrunning
 
+import java.io.Serializable
+
 
 // singleRun data 저장.
-//휴식일때는 0,0,휴식분
-//lapTime 은 0 제거하고 distance X laps. 구조를 좀더 생각해봐야됨.
 
-
-data class SingleRun(val date : String, val distance: List<Integer>, val laps : List<Integer>,
-                     val rest : List<Integer>, val lapTime : List<String>){
-}
-
-
-
-
-
+data class SingleRun (
+    val isRest : Boolean,
+    val distance : Int,
+    val set : Int,
+    val lapTimeMin : String,
+    val lapTimeSec : String,
+    val lapTimeMillisec : String):Serializable
